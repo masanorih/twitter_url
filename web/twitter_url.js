f041b9a879b9432032a19c376a0e11fce4a40df4
Masanori Hara <massa.hara@gmail.com>
2014-03-28 15:25:05 +0900

    forgot to add web to previous commit

--- web/twitter_url.js
+++ web/twitter_url.js
@@ -2,32 +2,56 @@
   "use strict";
   var twitter_url = angular.module('twitter_url', []);
   twitter_url.controller('Ctrl', function($scope, $http, $window) {
+    // controller methods
     // load url_list json data
     $scope.load_url_list = function() {
       var width = $window.innerWidth;
-      $http.get('/turl/list?width=' + width).success(function(data) {
+      var status = $scope.show_saved;
+      var post_data = {
+        width: width,
+        status: status
+      };
+      $http.post('/turl/list', post_data).success(function(data) {
           $scope.url_list = data;
       });
-      $http.get('/turl/count').success(function(data) {
+      $http.post('/turl/count', post_data).success(function(data) {
           $scope.url_count = data[0].count;
       });
     }
-    // controller methods
+    $scope.pass_all = function() {
+      for(var idx in $scope.url_list) {
+        var elem = $scope.url_list[idx];
+        $scope.pass(elem);
+      }
+    }
+    $scope.change_status = function() {
+        $scope.load_url_list();
+        //console.log("show_saved = " + $scope.show_saved);
+    }
     $scope.go = function(elem) {
       var id  = elem.id;
       var url = elem.url;
       $http.post('/turl/delete?id=' + id).success(function(data) {
-          delete_url_list(id);
+          delete_id(id);
       });
       window.open(url);
     }
     // simply delete entry
     $scope.pass = function(elem) {
       var id  = elem.id;
-      elem.show_deleting = "deleting...";
+      elem.show_inprogress = "passing...";
       $http.post('/turl/delete?id=' + id).success(function(data) {
-          delete_url_list(id);
-          delete elem.show_deleting;
+          delete_id(id);
+          delete elem.show_inprogress;
+      });
+    }
+    // simply delete entry
+    $scope.save = function(elem) {
+      var id  = elem.id;
+      elem.show_inprogress = "saving...";
+      $http.post('/turl/save?id=' + id).success(function(data) {
+          delete_id(id);
+          delete elem.show_inprogress;
       });
     }
 
@@ -40,7 +64,7 @@
       }
       return false;
     }
-    function delete_url_list(id) {
+    function delete_id(id) {
       var idx = get_idx(id);
       if (idx) {
         $scope.url_list.splice(idx, 1);
@@ -53,6 +77,7 @@
       }
     }
 
+    $scope.show_saved = 'list';
     $scope.load_url_list();
   });
 })(this);
